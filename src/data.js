de0a281bccc439812df6a8cea76c8454390febd0
Faiz Imam <isacsmile01@gmail.com>
2025-08-26 21:40:52 +0530

    song added

--- src/data.js
+++ src/data.js
@@ -5,4 +5,9 @@ export const defaultTracks = [
   { name: "Parkha Na", url: "/track/Parkhana.mp3" },
   { name: "Timro Pratiksa", url: "/track/TimroPratiksa.mp3" },
   { name: "Sahiba Tere", url: "/track/SahibaSong.mp3" },
+  { name: "Departure - Talha Anjum", url: "/track/Departure Lane - Talha Anjum.mp3" },
+{ name: "HASEEN", url: "/track/HASEEN - TALWIINDER.mp3" },
+{ name: "Slow Dancing In The Dark", url: "/track/Joji - Slow Dancing In The Dark.mp3" },
+{ name: "Pal Pal", url: "/track/Pal Pal.mp3" },
+{ name: "SHOR", url: "/track/SHOR-TalhaAnjummp3" },
 ];
