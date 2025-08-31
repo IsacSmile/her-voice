dbcff4dcab6bac8b79272cf12eb863217f013096
Faiz Imam <isacsmile01@gmail.com>
2025-08-31 16:44:29 +0530

    Move icons to public/icons for PWA

---
+++ app/src/main/java/app/vercel/her_voice_beta/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.her_voice_beta.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
