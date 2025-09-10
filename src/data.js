22c6c0326263efcf1e9497f4355a6b22eb76e04c
Faiz Imam <isacsmile01@gmail.com>
2025-09-11 02:38:15 +0530

    music added

--- src/data.js
+++ src/data.js
@@ -10,4 +10,18 @@ export const defaultTracks = [
   { id: "default-008", name: "Slow Dancing In The Dark", url: "/track/Joji - Slow Dancing In The Dark.mp3" },
   { id: "default-009", name: "Pal Pal", url: "/track/Pal Pal.mp3" },
   { id: "default-010", name: "SHOR", url: "/track/SHOR-TalhaAnjummp3" },
+  { id: "default-011", name: "kalandar Munawwar Faruqui", url: "/track/Kalandar (feat. Farhan Khan).mp4" },
+  { id: "default-012", name: "aleemrk - Cold Hours", url: "/track/aleemrk - Cold Hours (Lyrics).mp3" },
+  { id: "default-013", name: "AUR - Tu hai Kaha", url: "/track/AUR - Tu hai kahan.mp3" },
+  { id: "default-014", name: "KABHI KABHI - Aleemrk", url: "/track/KABHI KABHI - aleemrk (Official Audio).mp3" },
+  { id: "default-015", name: "LIFE GOES ON - Aleemrk", url: "/track/LIFE GOES ON - aleemrk (Official Audio).mp3" },
+  { id: "default-016", name: "SHOR-TalhaAnjum", url: "/track/SHOR-TalhaAnjum.mp3" },
+  { id: "default-017", name: "These Are the Days - Hostel Daze", url: "/track/These Are the Days.mp3" },
+  { id: "default-018", name: "Jaa heer ne tainu chod diya", url: "/track/Jaa heer ne tainu chod diya - Raanjhan.mp3" },
+  { id: "default-019", name: "XXXTENTACION - the remedy for the broken heart", url: "/track/XXXTENTACION - the remedy for the broken heart (Lyrics).mp3" },
+  { id: "default-020", name: "KR$NA - NO CAP", url: "/track/KR$NA - NO CAP (OFFICIAL VIDEO)  KALAMKAAR.mp4" },
+  { id: "default-021", name: "Agency - Talha Anjum", url: "/track/Agency - Talha Anjum.mp3" },
+  { id: "default-022", name: "Pehle Bhi Main", url: "/track/Pehle Bhi Main (Slowed  Reverb) .mp3" },
+
+
 ];
