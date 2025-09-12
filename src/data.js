f49008787df5a0183a79e3e7e8e1da5b053eae7e
Faiz Imam <isacsmile01@gmail.com>
2025-09-12 09:49:59 +0530

    some modification

--- src/data.js
+++ src/data.js
@@ -27,6 +27,8 @@ export const defaultTracks = [
   { id: "default-025", name: "Pehle Bhi Main", url: "/track/Pehle Bhi Main (Slowed  Reverb) .mp3" },
   { id: "default-026", name: "Thenkizhakku - Tamil Version", url: "/track/Thenkizhakku - Tamil Song.mp3" },
   { id: "default-027", name: "Wishlist -DinoJames", url: "/track/Wishlist-DinoJames.mp3" },
+  { id: "default-028", name: "Meet you at graveyard", src: "https://drive.google.com/file/d/1HXUUsCB9qSS-B6tAdsRF-nepsHssB9Pt/view?usp=drive_link"},
+
   
 
 
